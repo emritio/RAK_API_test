8e6ac1ab154f800ade1a5c0bafdc51c3af7dcbd1
mithesh06 <mitheshshankar2020@gmail.com>
2023-09-01 17:59:23 +0530

    1- Hid the keys, 2- Added JWT, 3- Added version

---
+++ client/api_keys.py
@@ -0,0 +1,2 @@
+db_key= 'postgresql://postgres:123@172.17.0.2:5432/postgres'
+jwt_key='your-secret-key'
--- client/apilog.py
+++ client/apilog.py
@@ -1,16 +1,19 @@
-from flask import Flask, request, render_template, redirect, url_for, jsonify
-from flask_sqlalchemy import SQLAlchemy
-#from config import SQLALCHEMY_DATABASE_URI
-import psycopg2
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-import datetime
-import bcrypt
+from flask import Flask, request, render_template, redirect, url_for, jsonify # Main flask framework
+from flask_sqlalchemy import SQLAlchemy  # Database setup
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity  # Web token setup
+import datetime # record time for login session
+import bcrypt # Hashing Password
 import api_keys #Python file that contains all the keys
 
-
+#Starts Flask
 app = Flask(__name__)
+#Importing database functions from flask
 app.config['SQLALCHEMY_DATABASE_URI'] =  api_keys.db_key
-app.config['JWT_SECRET_KEY'] =   api_keys.jwt_key# Change this to a strong secret key
+#Setting up tokens
+app.config['JWT_SECRET_KEY'] =   api_keys.jwt_key
+
+#Base Version 0-python.0-html.0-CSS (denotes changes from the base version)
+app.config['VERSION'] = '0.0.0'
 
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
@@ -18,18 +21,20 @@ jwt = JWTManager(app)
 with app.app_context():
     db.create_all()
 
+#The user table is created with required attributes
 class Users(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
     password = db.Column(db.String(120), nullable=False)
     last_login_time = db.Column(db.DateTime)
 
-
+#Function to verify credentials
 def check_password(username, password):
     user = Users.query.filter_by(username=username).first()
     if user:
         return user.password == password.encode('utf-8')
     return False
 
+#Function to keep track of login time for session time-out
 def record_login_time(username):
     user = Users.query.filter_by(username=username).first()
     if user:
@@ -41,25 +46,18 @@ def is_session_expired(last_login_time):
     current_time = datetime.datetime.now()
     return (current_time - last_login_time).total_seconds() > 120
 
-
+#The main page routes to index.html
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/dashboard/<username>', methods=['GET'])
-def dashboard(username):
-    user = Users.query.filter_by(username=username).first()
-    if user:
-        return render_template('dashboard.html', username=username)
-    return redirect(url_for('index'))
-
+#The function routes to the registration page for user info
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
         return render_template('register.html')
     
     elif request.method == 'POST':
-        #print('going inside post')
         if 'username' in request.form and 'password' in request.form:
             username = request.form['username']
             password = request.form['password']
@@ -67,7 +65,7 @@ def register():
             raise ValueError('Username not found in request')
 
         if Users.query.filter_by(username=username).first():
-            #print("It also takes the query")
+
             return jsonify({'error': 'Username already taken'}), 400
         
         # Hash the password
@@ -80,16 +78,15 @@ def register():
         
         return redirect(url_for('index'))
 
-
+#Login page to check credentials
 @app.route('/login', methods=['GET','POST'])
 def login():
-    # print("got inside login")
+    
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
-        # print("got inside post")
+        
         username = request.form['username']
-        # print("No error with request: ",username)
         password = request.form['password']
         user = Users.query.filter_by(username=username).first()
 
@@ -101,5 +98,14 @@ def login():
             return jsonify({'access_token': access_token}), 200,redirect(url_for('dashboard',usrename=username))
     return jsonify({'error': 'Invalid credentials'}), 401
 
+#Dashboard opens the home page
+@app.route('/dashboard/<username>', methods=['GET'])
+def dashboard(username):
+    user = Users.query.filter_by(username=username).first()
+    if user:
+        return render_template('dashboard.html', username=username)
+    return redirect(url_for('index'))
+
+#Main function starts the server on port 8001
 if __name__ == '__main__':
-    app.run(debug=True,port=8000)
+    app.run(debug=True,port=8001)
