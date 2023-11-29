3c2465ca45c9faa98d8b2ae8223f739d430c60f3
mithesh06 <131614258+mithesh06@users.noreply.github.com>
2023-11-30 01:20:45 +0530

    Delete client/python/db_maintenance.py

--- client/python/db_maintenance.py
+++
@@ -1,7 +0,0 @@
-from database import db
-
-def create_metric_id_column():
-    # Add the metric_id column to the system_metrics table
-    db.engine.execute(
-        'drop table SystemMetrics;drop table Users;'
-    )
